00f073f502bd8f4b366cea067687ef87b3de495a
Avenger <avengerorama@gmail.com>
2017-05-11 12:04:06 +0300

    sw

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Avenger on 11.05.2017.
+ */
+
+if(!('serviceWorker' in navigator))
+{
+	console.log('Service worker not supported');
+}
+else
+{
+	navigator.serviceWorker.register('/js/sw.js').then(registration => {
+		console.log('SW registered!');
+		console.log(registration);
+	}).catch(err => {
+		console.log(err);
+	});
+}
